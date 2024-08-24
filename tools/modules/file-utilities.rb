e5d6ebce6183d711eb23d631df3839f1a386f03e
florinhoinarescu <florin_hoinarescu@yahoo.com>
2024-08-24 19:12:47 +0300

    Update netlify.toml

---
+++ tools/modules/file-utilities.rb
@@ -0,0 +1,322 @@
+require_relative 'globals'
+require 'find'
+require 'jekyll'
+require 'fileutils'
+require 'liquid'
+
+module FileUtilities
+
+    def self.clear_and_create_empty_folder (folder_path)             
+        if Dir.exist?(folder_path)
+          FileUtils.rm_rf(Dir.glob("#{folder_path}/*"))
+        else
+          FileUtils.mkdir_p(folder_path)
+        end
+    end
+
+    def self.create_folder_if_not_exist (folder_path)             
+        if !Dir.exist?(folder_path)
+          FileUtils.mkdir_p(folder_path)
+        end
+    end
+
+    def self.clear_or_create_file(file_path)
+        File.open(file_path, "w") { |f| }
+    end
+      
+    def self.write_file(file_path, content)
+        if File.exist?(file_path)
+            File.open(file_path, "a") { |file| file.write(content) }
+        else
+            puts "OUPS!!! Can't find file #{file_path} ..."
+        end
+    end
+
+    def self.overwrite_file(file_path, content)
+        clear_or_create_file(file_path)
+        write_file(file_path, content)
+    end
+
+    # Function to check file existence (including relative paths)
+    def self.file_exists?(file_path, base_dir)
+        full_path = File.join(base_dir, file_path)
+        File.exist?(full_path)
+    end
+
+    # Function to check and report missing includes
+    def self.check_includes(source, content, base_dir, silent)
+        result = ""
+        brokeIncludes = 0
+        content.scan(/\{% include(?:_relative)? (.*) %}/) do |match|
+            file_path =match[0].split(" ", 0)[0]
+            file_extension = File.extname(file_path)
+            if (file_extension != ".liquid")
+                unless file_exists?(file_path, base_dir)
+                    result = "#{source}: Missing include: #{file_path} (in #{base_dir})"
+                    Globals.putsColText(Globals::YELLOW, " - #{result}") if !silent
+                    write_file("#{Globals::ROOT_DIR}/tools/checks/broken-includes.log", "#{result}\n")
+                    brokeIncludes += 1
+                end
+            else
+                unless File.exist?("#{Globals::ROOT_DIR}/_includes/"+file_path)
+                    fileName = File.basename(("#{Globals::ROOT_DIR}/_includes/"+file_path))
+                    result = "#{source}: Missing include: #{fileName} (in #{Globals::ROOT_DIR}/_includes/#{file_path})"
+                    Globals.putsColText(Globals::YELLOW, " - #{result}") if !silent
+                    write_file("#{Globals::ROOT_DIR}/tools/checks/broken-includes.log", "#{result}\n")
+                    brokeIncludes += 1
+                end
+            end
+        end
+        return brokeIncludes
+    end
+
+    def self.getFileFromPermalink(docsDir, permalink)
+        Find.find(docsDir) do |path|
+            next unless File.file?(path) && (path.end_with?('.html') || path.end_with?('.md'))
+            front_matter, _ = File.read(path).split('---')[1..2]
+            page_data = front_matter ? YAML.safe_load(front_matter) : ""
+            return path if page_data && page_data['permalink'] == permalink
+        end
+       
+    end
+
+    def self.parse_front_matter(content)
+        if content =~ /\A(---\s*\n.*?\n?)^((---|\.\.\.)\s*$\n?)/m
+          front_matter = YAML.safe_load($1)
+          content = $'
+          [front_matter, content]
+        else
+          [nil, content]
+        end
+    end
+
+    def self.extract_main_content(site, rendered_content)
+        doc = Nokogiri::HTML(rendered_content)
+        tags_to_remove = site.data['buildConfig']["tagsToRemoveOnDryRender"]
+        tags_to_remove.each do |tag|
+            doc.search(tag).remove
+          end
+        main_content = doc.css('main').text
+        main_content.strip
+    end
+
+    def self.render_jekyll_page(site, file_path, front_matter, content_body)
+        # Create a temporary page to render
+        page = Jekyll::PageWithoutAFile.new(site, site.source, File.dirname(file_path), File.basename(file_path))
+        page.content = content_body
+        page.data = front_matter
+  
+        # Determine the layout to use (default to 'default' layout if not specified)
+        layout_name = front_matter['layout'] || 'default'
+  
+        # Find the layout
+        layout = find_layout(site, layout_name)
+        raise "Layout '#{layout_name}' not found." unless layout
+  
+        # Assign the layout to the page
+        page.data['layout'] = layout_name
+
+        # Render the page with the assigned layout
+        page.render(site.layouts, site.site_payload)
+        extract_main_content(site, page.output)
+    end
+  
+    # Method to find a layout (including from themes)
+    def self.find_layout(site, layout_name)
+        # First, check if the layout exists in the site's layouts
+        layout = site.layouts[layout_name]
+
+        # If not found in site layouts, check in theme layouts
+        if layout.nil? && site.theme&.layouts
+            layout = site.theme.layouts[layout_name]
+        end
+        layout
+    end
+
+    def self.valid_front_matter?(content)
+        content =~ /\A---\s*\n.*?\n---\s*\n/m
+    end
+
+    def self.file_raw_content_needs_update(site, current_raw_content, file_front_matter)
+        raw_content_file_path = "#{site.data["buildConfig"]["rawContentFolder"]}/#{file_front_matter["permalink"].gsub("/", "_")}.txt"
+        return true if !File.exist?(raw_content_file_path)
+        saved_raw_content = File.read(raw_content_file_path)
+        return true if saved_raw_content != current_raw_content
+        return false
+    end
+
+    def self.generate_raw_content(site)
+        Globals.show_spinner do
+            doc_contents_dir = File.join(site.source, Globals::DOCS_ROOT)
+            FileUtilities.create_folder_if_not_exist (site.data['buildConfig']["rawContentFolder"])
+            Globals.newLine
+            numPages = 0
+            modified_files = []
+            modified_files_object = {
+                "files" => modified_files
+            }
+
+            FileUtilities.overwrite_file(
+                "#{site.data["buildConfig"]["rawContentFolder"]}/modified_files.json", 
+                modified_files_object.to_json
+            )
+
+            Dir.glob(File.join(doc_contents_dir, '**', '*.{md, html}')).each do |file_path|
+                front_matter, content = FileUtilities.parse_front_matter(File.read(file_path)) || {}
+                next if front_matter == {}
+                next if front_matter.nil? || front_matter.empty?
+                next if content.nil? || content.empty?
+                next if front_matter["permalink"].nil? || front_matter["permalink"].empty?
+                next if front_matter["title"].nil? || front_matter["title"].empty?
+                page = Globals.find_object_by_multiple_key_value(JSON.parse(site.data['page_list']), {"permalink" => front_matter["permalink"]}) || {}
+                next if page.nil? || page == {}
+                Globals.moveUpOneLine
+                Globals.putsColText(Globals::PURPLE,"Generating raw content ... #{front_matter["permalink"]}")
+                Globals.show_spinner do
+                    rendered_content = FileUtilities.render_jekyll_page(site, file_path, front_matter, content)
+                    text_content = Globals.text_pre_process(Nokogiri::HTML(rendered_content).text)
+                    if (FileUtilities.file_raw_content_needs_update(site, text_content, front_matter ))
+                        
+                        FileUtilities.overwrite_file(
+                            "#{site.data["buildConfig"]["rawContentFolder"]}/#{front_matter["permalink"].gsub("/", "_")}.txt", 
+                            text_content
+                        )
+                        modified_files << "#{site.data["buildConfig"]["rawContentFolder"]}/#{front_matter["permalink"].gsub("/", "_")}.txt"
+                        modified_files_object = {
+                            "files" => modified_files
+                        }
+
+                        FileUtilities.overwrite_file(
+                            "#{site.data["buildConfig"]["rawContentFolder"]}/modified_files.json",
+                            JSON.pretty_generate(modified_files_object)
+                        )
+                        numPages +=1
+                    end
+                end
+                Globals.clearLine # clear whatever spinner character is still visible   
+            end
+            Globals.moveUpOneLine
+            Globals.clearLine
+            if (numPages > 0 )
+                Globals.putsColText(Globals::PURPLE,"Generating raw content ... done (#{numPages} page(s) changed)")
+            else
+                Globals.putsColText(Globals::PURPLE,"Generating raw content ... nothing to do! (no content chages)")
+            end
+        end
+    end
+
+    def self.read_json_file(file_path)
+        begin
+          file_contents = File.read(file_path)
+          data = JSON.parse(file_contents)
+          return data
+        rescue Errno::ENOENT
+          puts "File not found: #{file_path}"
+          return nil
+        rescue JSON::ParserError
+          puts "Error parsing JSON file: #{file_path}"
+          return nil
+        end
+    end
+      
+    # Function to extract the included file paths from content
+    def self.extract_included_paths(file_path, content)
+        included_paths = []
+
+        pattern = /
+            \{\%\s*                                  
+            (ExternalSiteContent|ExternalSiteContentMM)\s*  
+            \{                                       
+            .*?                                      
+            "file_path"\s*:\s*"([^"]+)"              
+            .*?                                      
+            \}\s*\%\}                                
+        /mx                                        
+        matches = content.scan(pattern)
+        included_paths = matches.map { |match| match[1] }
+      
+        pattern = /
+            \{\%\s*                              
+            (include_relative|include)\s*         
+            (?:"([^"]+)"|'([^']+)'|([^"'\s]+))   
+            \s*\%\}                              
+        /x                                                 
+          
+        matches = content.scan(pattern)
+        file_paths = matches.map do |match|
+            match[1] || match[2] || match[3]  # Prioritize capturing groups based on quote presence
+        end.compact.uniq || []
+
+        base_path = "#{Globals::DOCS_DIR}/"
+        relative_path = "#{File.dirname(file_path.sub(base_path, ''))}/"
+        # create full relative paths since {% include %} works only with relative path to the current file and not with relative path to the root dir 
+        file_paths.map! { |value| relative_path + value }
+        file_paths.map! { |element| element.sub(/\A\.\.?\//, '') } # remove ./ or ../ from paths
+
+        #puts "#{file_path} #{file_paths}"
+        (included_paths | file_paths).compact.uniq
+    end
+
+    def self.generate_doc_dependencies(site)
+        Globals.show_spinner do
+            Globals.putsColText(Globals::PURPLE,"Generating dependencies ...")
+            doc_contents_dir = File.join(site.source, Globals::DOCS_ROOT)
+            relationships = Hash.new { |hash, key| hash[key] = [] }
+            numPages = 0
+            Dir.glob("#{doc_contents_dir}/**/*.md").each do |file_path|
+                front_matter, content = parse_front_matter(File.read(file_path))
+                next unless front_matter
+                permalink = front_matter["permalink"]
+                next unless permalink
+                page = Globals.find_object_by_multiple_key_value(JSON.parse(site.data['page_list']), {"permalink" => permalink}) || {}
+                next unless page != {}
+                
+                Globals.moveUpOneLine
+                Globals.putsColText(Globals::PURPLE,"Generating dependencies ... #{permalink}")
+                included_paths = extract_included_paths(file_path, content)
+
+                included_paths.each do |included_path|
+                    # Convert the included_path to its permalink
+                    included_file_path = File.join(doc_contents_dir, included_path)
+                    included_front_matter, _ = parse_front_matter(File.read(included_file_path)) if File.exist?(included_file_path)
+                    next unless included_front_matter
+                    included_permalink = included_front_matter["permalink"]
+                    next unless included_permalink
+                    included_page = Globals.find_object_by_multiple_key_value(JSON.parse(site.data['page_list']), {"permalink" => permalink}) || {}
+                    next unless included_page != {}
+
+                    relationships[included_permalink] << permalink
+                end
+                numPages +=1
+            end
+
+            # Convert the relationships hash to the desired JSON array format
+            result = relationships.map do |source_permalink, target_permalinks|
+                { permalink: source_permalink, imported_by: target_permalinks }
+            end
+            #puts JSON.pretty_generate(result)
+            FileUtilities.overwrite_file(
+                "#{site.data["buildConfig"]["rawContentFolder"]}/dependencies.json", 
+                JSON.pretty_generate(result)
+            )
+            Globals.moveUpOneLine
+            Globals.clearLine
+            Globals.putsColText(Globals::PURPLE,"Generating dependencies ... done (#{numPages} pages)")
+        end
+    end
+
+    def self.get_real_files_from_raw_content_files(file_paths)
+        file_paths.map do |path|
+          file_name = File.basename(path, ".*") 
+          transformed_name = file_name.gsub('_', '/')
+          getFileFromPermalink(Globals::DOCS_DIR, transformed_name)
+        end
+    end
+
+    def self.get_real_file_from_raw_content_file(file_path)
+        file_name = File.basename(file_path, ".*") 
+        transformed_name = file_name.gsub('_', '/')
+        getFileFromPermalink(Globals::DOCS_DIR, transformed_name)
+    end
+
+end
---
+++ tools/modules/globals.rb
@@ -0,0 +1,190 @@
+require 'thread'
+require 'yaml'
+require 'dotenv'
+require 'securerandom'
+Dotenv.load
+
+module Globals
+    RESET = "\e[0m"
+    WHITE = "\e[37m"
+    YELLOW = "\e[33m"
+    GREEN = "\e\n[32m"
+    PURPLE = "\e[35m"
+    RED = "\e[31m"
+    BACK_1_ROW = "\e[1A"
+    ARROW_RIGHT = "\u2192"
+
+    # Spinner character sequence
+    CHARS = %w(- \\ | /)
+    BLANK_CHARS = ['']
+
+    # removing last 15 chars '/tools/modules/'
+    ROOT_DIR = __dir__[0..-15]
+    DOCS_DIR = "#{ROOT_DIR}/doc-contents"
+    DOCS_ROOT = "doc-contents"
+
+    # config files
+    CONFIG_YML = "#{ROOT_DIR}/_config.yml"
+    SITECONFIG_YML = "#{ROOT_DIR}/_data/siteConfig.yml"
+    PAGECONFIG_YML = "#{ROOT_DIR}/_data/pageConfig.yml"
+    LOG_FILE = "#{ROOT_DIR}/tools/checks/check.log"
+    
+    def self.putsColText(col, text)
+        puts col + text + RESET
+    end
+
+    def self.clearLine()
+        #print "\e[2K"
+        print "\e[2K\e[G"
+
+    end
+
+    def self.moveUpOneLine()
+        print "\e[1A"
+    end
+
+    def self.newLine()
+        puts ""
+    end
+
+    def self.clearConsole()
+        puts "\e[H\e[2J"
+    end
+
+    def self.removeFirstAndLastSlash(str)
+        if (str[0] == '/')
+            str = str[1..-1]
+        end
+        if str[-1] == '/'
+            str = str[0..-2]
+        end
+        return str
+    end
+
+    def self.removeSlashesFromArrayElements(ar)
+        # ignore nil elements, otherwise error will be raised
+        modified_ar = ar.compact.map do |element|
+            # Modify the element here
+            modified_element = removeFirstAndLastSlash(element)
+            # Return the modified element
+            modified_element
+        end
+        return modified_ar
+    end
+
+    def self.show_spinner(interval = 0.1)
+    # Create a new thread for the spinner
+    spinner_thread = Thread.new do
+        loop do
+            if (ENV["CONSOLE_BLANK_SPINNER_CHARS"] != "true")
+                CHARS.each do |char|
+                    print char
+                    sleep interval
+                    print "\b"
+                end
+            else
+                BLANK_CHARS.each do |char|
+                    print char
+                    sleep interval
+                    print "\b"
+                end
+            end
+        end
+    end
+
+    # Allow the main thread to continue execution
+    yield
+
+    # Stop the spinner thread when done
+    spinner_thread.kill
+    end
+
+    def self.match_liquid_variable?(string)
+        # Pattern with optional whitespace at the beginning and end
+        pattern = /\A\s*\{\{ site\.(?:\w+\.)*\w+ \}\}(\s*$)?/
+        string =~ pattern
+    end
+
+    def self.find_value_in_yaml(yaml_file, path, excludePath)
+        config = YAML.load_file(yaml_file)
+        keys = path.sub(excludePath, '').split(".")
+        value = "not_found"
+        keys.reduce(config) do |hash, key|
+          value = hash.is_a?(Hash)? hash[key] : "not_found:#{yaml_file}"
+        end
+        return value
+    end
+
+    def self.getArrayDuplicates(ar)
+        occurrences = Hash.new(0)
+        ar.each { |element| occurrences[element] += 1 }
+        duplicates = occurrences.select { |_, count| count > 1 }.keys
+        return duplicates
+    end
+
+    def self.removeFrontMatter(content)
+        return content.gsub(/^---.*?---\n/m, '')
+    end
+
+    def self.find_object_by_key_value(objArray, key, value)
+        objArray.find { |item| item["#{key}"] == value }
+    end
+
+    def self.find_object_by_multiple_key_value(objArray, criteria)
+        objArray.find do |obj|
+            criteria.all? { |key, value| obj[key] == value }
+        end
+    end
+
+    def self.text_pre_process(text)
+        
+        text = text.downcase # everyhing to lowercase
+        text = text
+            .gsub(/\{\{([^}]*)\}\}/, "")
+            .gsub(/\{\{([^}]*)/, "")
+            .gsub(/([^}]*)\}\}/, "")    # remove all not rendered tags
+        text = text.gsub(/\s\s+/, ". ") # better mark sentences, replace multiple spaces with '. '
+        text = text.gsub(/\n\s+/, "")
+        text = text.gsub(/\.{2,}/, '.') # replace multiple dots with single dot
+
+        text = text.delete("\n\r\t")
+
+        text.strip
+    end
+
+    def self.run_python_script(site, python_script, script_parameter, callback)
+        python_script_with_param = "#{site.data["buildConfig"]["pyLaunch"]} #{python_script} '#{script_parameter.to_json}'"
+        outputNo = 0
+        Open3.popen3(python_script_with_param) do |stdin, stdout, stderr, wait_thr|
+            stdout.each do |line|
+                begin
+                    response = JSON.parse(line)
+                    outputNo += 1
+                    callback.call(
+                        {
+                            "outputNo" => outputNo,
+                            "payload" => response
+                        }
+                    )
+                rescue JSON::ParserError => e
+                    puts "Failed to parse JSON returned by #{python_script}: #{e.message}"
+                end
+            end
+    
+            exit_status = wait_thr.value
+            unless exit_status.success?
+                puts "Error running #{python_script}: #{stderr.read}"
+            end
+        end
+    end
+
+    def self.find_object_key_value(objects, criteria, return_key)
+        found_object = find_object_by_multiple_key_value(objects, criteria)        
+        found_object ? found_object[return_key] : nil
+    end
+
+    def self.uuid_gen()
+        SecureRandom.uuid.gsub('-', '')[0, 10]    
+    end
+      
+end
