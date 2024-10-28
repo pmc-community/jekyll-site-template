850465b8df7c04fe1c776b4245eb692e93368a8b
florinhoinarescu <florin_hoinarescu@yahoo.com>
2024-10-28 10:09:38 +0000

    Remove all files

--- assets/js/iframe-global.js
+++
@@ -1,8 +0,0 @@
-const iframeWindow = window;
-const iframeDocument = iframeWindow.document;
-const mainJQuery = window.parent.mainJQuery;
-iframeWindow.$ = mainJQuery;
-iframeWindow.jQuery = mainJQuery;
-
-const mainLodash = window.parent._;
-iframeWindow._ = mainLodash;
