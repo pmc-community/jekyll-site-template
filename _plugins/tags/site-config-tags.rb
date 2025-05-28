1df7960bfb0d31a512e51d8be773213326e1f2ca
Florin <florin_hoinarescu@yahoo.com>
2025-05-28 11:16:57 +0300

    Create site-config-tags.rb

---
+++ _plugins/tags/site-config-tags.rb
@@ -0,0 +1,35 @@
+require 'dotenv'
+
+Dotenv.load
+
+module Jekyll
+
+    module SiteConfigSettings
+
+        class SiteLanguage < Liquid::Tag
+  
+            def initialize(tag_name, input, context)
+                super
+                @input = input
+            end
+
+            def render(context)
+                langSettings = context.registers[:site].data["siteConfig"]["multilang"]
+                siteLang = ""
+                if (ENV["DEPLOY_ENV"] != 'production'  )
+                    siteLang = ""                     
+                else
+                    if (langSettings["siteLanguage"] == 0)
+                        siteLang = ""
+                    else
+                        siteLang = langSettings["availableLang"][langSettings["siteLanguage"]]["lang"]
+                    end      
+                end
+                siteLang
+            end
+        end
+
+    end
+end
+  
+Liquid::Template.register_tag('SiteLanguage', Jekyll::SiteConfigSettings::SiteLanguage)
