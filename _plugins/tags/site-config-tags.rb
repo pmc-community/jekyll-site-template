0477870e785e5362b13df3ddc5f1531ae68085ee
Florin <florin_hoinarescu@yahoo.com>
2025-05-30 19:29:56 +0300

    Update site-config-tags.rb

--- _plugins/tags/site-config-tags.rb
+++ _plugins/tags/site-config-tags.rb
@@ -1,3 +1,6 @@
+
+require_relative "../../tools/modules/globals"
+require_relative "../../tools/modules/file-utilities"
 require 'dotenv'
 
 Dotenv.load
@@ -34,7 +37,7 @@ module Jekyll
 
 
         class SiteLanguageForSearch < Liquid::Tag
-  
+
             def initialize(tag_name, input, context)
                 super
                 @input = input
@@ -45,6 +48,50 @@ module Jekyll
                 siteLang = langSettings["availableLang"][langSettings["siteLanguage"]]["lang"]
                 siteLang
             end
+
+        end
+
+        class SiteLanguagesExceptActiveLanguage < Liquid::Tag
+
+            def initialize(tag_name, input, context)
+                super
+                @input = input
+            end
+
+            def render(context)
+                langSettings = context.registers[:site].data["siteConfig"]["multilang"]
+                siteLang = langSettings["availableLang"][langSettings["siteLanguage"]]["lang"]
+                allLanguages = langSettings["availableLang"]
+                filtered = allLanguages.reject { |item| item["lang"] == siteLang }
+                noTranslation = []
+                
+                filtered.each do |language|
+                    if (FileUtilities.file_exists?("assets/locales/#{language["lang"]}.json", Globals::ROOT_DIR))
+                        noTranslation << language["lang"]
+                    end
+                end
+
+                result = filtered.reject { |item| !noTranslation.include?(item["lang"]) }
+                result
+            end
+            
+        end
+
+        class SiteDefaultLanguageCode < Liquid::Tag
+
+            def initialize(tag_name, input, context)
+                super
+                @input = input
+            end
+
+            def render(context)
+                langSettings = context.registers[:site].data["siteConfig"]["multilang"]
+                availableLang = langSettings["availableLang"]
+                defaultLanguage = langSettings["fallbackLang"]
+                defaultLanguageCode = availableLang[defaultLanguage]["lang"]
+                defaultLanguageCode
+            end
+
         end
 
     end
@@ -52,3 +99,5 @@ end
   
 Liquid::Template.register_tag('SiteLanguage', Jekyll::SiteConfigSettings::SiteLanguage)
 Liquid::Template.register_tag('SiteLanguageForSearch', Jekyll::SiteConfigSettings::SiteLanguageForSearch)
+Liquid::Template.register_tag('SiteLanguagesExceptActiveLanguage', Jekyll::SiteConfigSettings::SiteLanguagesExceptActiveLanguage)
+Liquid::Template.register_tag('SiteDefaultLanguageCode', Jekyll::SiteConfigSettings::SiteDefaultLanguageCode)
