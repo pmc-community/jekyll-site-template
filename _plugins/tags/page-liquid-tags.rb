3c835872298c3c7663b338689a1e4ba05ec3ac7b
Florin <florin_hoinarescu@yahoo.com>
2025-06-12 14:33:14 +0300

    Update page-liquid-tags.rb

--- _plugins/tags/page-liquid-tags.rb
+++ _plugins/tags/page-liquid-tags.rb
@@ -2,6 +2,7 @@ require 'json'
 require_relative "../../tools/modules/globals"
 require_relative "../../tools/modules/permalinks-utilities"
 require_relative "../../tools/modules/col-utilities"
+require_relative "../../tools/modules/file-utilities"
 
 module Jekyll
 
@@ -258,6 +259,56 @@ module Jekyll
             
         end
 
+        class DryRenderPage < Liquid::Tag
+            def initialize(tag_name, input, tokens)
+                super
+                @input = input.strip
+            end
+
+            def render(context)
+                begin
+                    site = context.registers[:site]
+
+                    # Step 1: Resolve input (literal or variable)
+                    evaluated_input = Liquid::Template.parse(@input).render(context).strip
+                    liquid_source = context[evaluated_input] || evaluated_input
+
+                    # Step 2: Render inner Liquid
+                    liquid_rendered = FileUtilities.render_liquid_string(site, liquid_source)
+
+                    # Step 3: Convert Markdown to HTML
+                    markdown_converter = site.find_converter_instance(Jekyll::Converters::Markdown)
+                    html = markdown_converter.convert(liquid_rendered)
+
+                    # Step 4: Replace headings <h1>–<h6> with styled <p> tags
+                    # to not mess around with the FAQ ToC
+                    replacements = {
+                        'h1' => 'p class="fw-demibold fs-2"',
+                        'h2' => 'p class="fw-demibold fs-3"',
+                        'h3' => 'p class="fw-demibold fs-4"',
+                        'h4' => 'p class="fw-demibold fs-5"',
+                        'h5' => 'p class="fw-demibold fs-6"',
+                        'h6' => 'p class="fw-demibold fs-6"'
+                    }
+
+                    replacements.each do |tag, replacement|
+                        # Use non-greedy match and allow whitespace/newlines
+                        html.gsub!(/<#{tag}[^>]*?>(.*?)<\/#{tag}>/im) do
+                        content = Regexp.last_match(1).strip
+                        "<#{replacement}>#{content}</p>"
+                        end
+                    end
+
+                    rendered_content = html
+
+                rescue => e
+                    puts "[DryRenderPage ERROR] #{e.message}"
+                    rendered_content = "Something went wrong during dry rendering ..."
+                end
+
+                rendered_content
+            end
+        end
     end
 end
   
@@ -269,4 +320,5 @@ Liquid::Template.register_tag('PageRelated', Jekyll::SitePages::PageRelated)
 Liquid::Template.register_tag('PageSimilarByContent', Jekyll::SitePages::PageSimilarByContent)
 Liquid::Template.register_tag('PageAutoSummary', Jekyll::SitePages::PageAutoSummary)
 Liquid::Template.register_tag('PageCollection', Jekyll::SitePages::PageCollection)
+Liquid::Template.register_tag('DryRenderPage', Jekyll::SitePages::DryRenderPage)
   
