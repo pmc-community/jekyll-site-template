d3215f9ec39de6ca0ee6f43281d3dea835114071
Florin <florin_hoinarescu@yahoo.com>
2025-07-13 14:46:56 +0300

    update

--- _plugins/generators/similar-by-content-gen.rb
+++ _plugins/generators/similar-by-content-gen.rb
@@ -4,115 +4,124 @@ require 'open3'
 
 module Jekyll
 
-    class PageSimilarByContent < Generator
-      safe true
-      priority :normal # must be after pageList generator from _plugins/generators/pages-gen.rb
-  
-        def generate(site)
+  class PageSimilarByContent < Generator
+    safe true
+    priority :normal # must run after pageList generator
 
-            # GENERATE SIMILAR BY CONTENT
-            if (site.data['buildConfig']["pyEnable"] && site.data['buildConfig']["pySimilarPagesByContent"]["enable"])
-                
-                modified_files = FileUtilities.read_json_file("#{site.data['buildConfig']["rawContentFolder"]}/modified_files.json")["files"]
-                return if !modified_files
-                if (modified_files.length == 0)
-                    Globals.putsColText(Globals::PURPLE,"Generating similar by content ... nothing to do! (no content changes)")            
-                else
+    def generate(site)
 
-                    # generating the full list of pages for which similarByContent will be generated
-                    # full list of pages = modified_files + pages that had the as similar one of the modified_files
-                    
-                    if File.exist?("#{site.data['buildConfig']["rawContentFolder"]}/autoSimilar.json")
-                        current_similar_pages = FileUtilities.read_json_file("#{site.data['buildConfig']["rawContentFolder"]}/autoSimilar.json") || []
-                    else
-                        current_similar_pages = []
-                    end
+      # === GENERATE SIMILAR BY CONTENT ===
+      if site.data['buildConfig']["pyEnable"] &&
+         site.data['buildConfig']["pySimilarPagesByContent"]["enable"]
 
-                    modified_files_permalinks = modified_files.map do |file_path|
-                        File.basename(file_path, ".txt").gsub('_', '/')
-                    end
+        modified_path = "#{site.data['buildConfig']["rawContentFolder"]}/modified_files.json"
+        modified_data = FileUtilities.read_json_file(modified_path)
+        modified_files = modified_data["files"] rescue nil
 
-                    files_to_be_processed = modified_files
-                    #puts modified_files_permalinks
+        return unless modified_files
+        if modified_files.empty?
+          Globals.putsColText(Globals::PURPLE, "Generating similar by content ... nothing to do! (no content changes)")
+        else
+          auto_similar_path = "#{site.data['buildConfig']["rawContentFolder"]}/autoSimilar.json"
+          current_similar_pages = if File.exist?(auto_similar_path)
+            FileUtilities.read_json_file(auto_similar_path) || []
+          else
+            []
+          end
 
-                    modified_files_permalinks.each do |permalink|
-                        if (current_similar_pages.length > 0)
-                            similar_pages = Globals.find_object_by_multiple_key_value(current_similar_pages, {"permalink" => permalink})["similarFiles"] || []
-                        else
-                            similar_pages = []
-                        end
-                        
-                        processed_similar_pages = similar_pages.map do |path|
-                            "doc-raw-contents/#{path.gsub('/', '_')}.txt"
-                        end
-                        files_to_be_processed = files_to_be_processed + processed_similar_pages
-                    end
+          # Get permalinks for modified files
+          modified_permalinks = modified_files.map do |file_path|
+            File.basename(file_path, ".txt").gsub('_', '/')
+          end
 
-                    # files_to_be_processed contains the full list of pages for which similarByContent will be generated
-                    # files_to_be_processed is sent as fileList array in the json parameter to the python script
-                    files_to_be_processed = files_to_be_processed.compact.uniq
+          files_to_process = modified_files.dup
 
-                    return if !files_to_be_processed
-                    return if files_to_be_processed.length == 0
-                    # SUMMARIES
-                    Globals.show_spinner do
-                        Globals.putsColText(Globals::PURPLE,"Generating similar by content ... for #{files_to_be_processed.length} pages")
-                        json_input = { "pageList" => site.data['page_list'], "fileList" => files_to_be_processed}
-                        python_script = site.data["buildConfig"]["pySimilarPagesByContent"]["script"]
+          # Add any pages that were previously marked as similar to the modified ones
+          modified_permalinks.each do |permalink|
+            similar_obj = Globals.find_object_by_multiple_key_value(current_similar_pages, { "permalink" => permalink })
+            similar_pages = similar_obj ? (similar_obj["similarFiles"] || []) : []
+            processed_paths = similar_pages.map { |p| "doc-raw-contents/#{p.gsub('/', '_')}.txt" }
+            files_to_process += processed_paths
+          end
 
-                        page_similarByContent_callback = Proc.new do |python_script_response|
-                            permalink = python_script_response["payload"]["payload"]["permalink"] || ""
-                            pageNo = python_script_response["outputNo"] if permalink != ""
-                            Globals.clearLine
-                            Globals.putsColText( Globals::PURPLE, "- PERMALINK: #{permalink} ... done (#{pageNo})") if permalink != ""
-                            Globals.putsColText( Globals::PURPLE, "#{python_script_response["payload"]["message"]}") if permalink == ""
-                        end
+          files_to_process = files_to_process.compact.uniq
+          return if files_to_process.empty?
 
-                        Globals.run_python_script(site, python_script, json_input, page_similarByContent_callback)
-                    end
-                
-                    Globals.clearLine
-                end
+          # Run Python script
+          Globals.show_spinner do
+            Globals.putsColText(Globals::PURPLE, "Generating similar by content ... for #{files_to_process.length} pages")
+
+            json_input = {
+              "pageList" => site.data['page_list'],
+              "fileList" => files_to_process
+            }
+
+            python_script = site.data["buildConfig"]["pySimilarPagesByContent"]["script"]
+
+            callback = Proc.new do |response|
+              permalink = response.dig("payload", "payload", "permalink") || ""
+              pageNo = response["outputNo"] if !permalink.empty?
+
+              Globals.clearLine
+              if !permalink.empty?
+                Globals.putsColText(Globals::PURPLE, "- PERMALINK: #{permalink} ... done (#{pageNo})")
+              else
+                Globals.putsColText(Globals::PURPLE, response.dig("payload", "message") || "(No message)")
+              end
             end
 
-            # UPDATE PAGE LIST
-            autoSimilar = [];
-            autoSimilar_path = "#{site.data["buildConfig"]["rawContentFolder"]}/autoSimilar.json"
-            return if !File.exist?(autoSimilar_path)
-            autoSimilar = File.read(autoSimilar_path)
-            begin
-                autoSimilar_json = JSON.parse(autoSimilar)
-            rescue
-                Globals.putsColText(Globals::RED, "- Cannot parse #{site.data["buildConfig"]["rawContentFolder"]}/autoSimilar.json")
-            end
-            pageList = JSON.parse(site.data['page_list'])
-            autoSimilar = autoSimilar_json
-            autoSimilar.each do |pageObj|
-                pageList.each do |page|
-                    if page["permalink"] == pageObj["permalink"]
-                        similarPages = []
-                        pageObj["similarFiles"].each do |similarPage|
-                            similarPageObj =  Globals.find_object_by_multiple_key_value(
-                                JSON.parse(site.data['page_list']), 
-                                {"permalink" => similarPage}
-                            ) || {}
-                            
-                            if (similarPageObj != {})
-                                similarPages << {
-                                    "permalink" => similarPageObj["permalink"],
-                                    "title" => similarPageObj["title"]
-                                }
-                                
-                            end
-                        end
-                        page["similarByContent"] = similarPages
-                        break
-                    end
-                end
-            end
-            site.data['page_list'] = pageList.to_json
+            Globals.run_python_script(site, python_script, json_input, callback)
+          end
 
+          Globals.clearLine
         end
-    end
+      end
 
-end  
+      # === UPDATE PAGE LIST WITH SIMILARITY RESULTS ===
+      auto_similar_path = "#{site.data["buildConfig"]["rawContentFolder"]}/autoSimilar.json"
+      return unless File.exist?(auto_similar_path)
+
+      begin
+        auto_similar = JSON.parse(File.read(auto_similar_path))
+      rescue
+        Globals.putsColText(Globals::RED, "- Cannot parse #{auto_similar_path}")
+        return
+      end
+
+      begin
+        page_list = JSON.parse(site.data['page_list'])
+      rescue
+        Globals.putsColText(Globals::RED, "- Cannot parse page list from site.data['page_list']")
+        return
+      end
+
+      auto_similar.each do |page_obj|
+        page_list.each do |page|
+          next unless page["permalink"] == page_obj["permalink"]
+
+          similar_pages = []
+
+          (page_obj["similarFiles"] || []).each do |similar_permalink|
+            similar_page_obj = Globals.find_object_by_multiple_key_value(
+              page_list,
+              { "permalink" => similar_permalink }
+            ) || {}
+
+            unless similar_page_obj.empty?
+              similar_pages << {
+                "permalink" => similar_page_obj["permalink"],
+                "title" => similar_page_obj["title"]
+              }
+            end
+          end
+
+          page["similarByContent"] = similar_pages
+          break
+        end
+      end
+
+      site.data['page_list'] = page_list.to_json
+    end
+  end
+
+end
