74af8925e9bfc54d3a9bdfc442d217fd1ac38a39
Florin <florin_hoinarescu@yahoo.com>
2025-07-14 18:27:38 +0300

    Update utility-tags.rb

--- _plugins/tags/utility-tags.rb
+++ _plugins/tags/utility-tags.rb
@@ -1,4 +1,5 @@
 require_relative "../../tools/modules/globals"
+require_relative "../../tools/modules/file-utilities"
 
 Dotenv.load
 
@@ -18,8 +19,24 @@ module Jekyll
             end
         end
 
+        class DirStructure < Liquid::Tag
+  
+            def initialize(tag_name, input, context)
+                super
+                @input = input
+            end
+
+            def render(context)
+                param = Liquid::Template.parse(@input).render(context)
+                structure = FileUtilities.generate_folder_structure("#{Dir.pwd}/#{param.strip}/")
+                structure
+            end
+            
+        end
+
     end
 
 end
   
 Liquid::Template.register_tag('UUID', Jekyll::Utilities::UUID)
+Liquid::Template.register_tag('DirStructure', Jekyll::Utilities::DirStructure)
