9dc8dc1c799c41abaab91ba97c737bcb5e4193b6
Florin <florin_hoinarescu@yahoo.com>
2025-10-11 18:31:33 +0300

    update

---
+++ _plugins/hooks/update-just-the-docs-js-post-write.rb
@@ -0,0 +1,21 @@
+require_relative "../../tools/modules/globals"
+require_relative "../../tools/modules/file-utilities"
+require 'dotenv'
+Dotenv.load
+
+Jekyll::Hooks.register :site, :post_write do |site|
+    algoliaSearch = ENV["ALGOLIA_SEARCH_ENABLED"]
+    algoliaCustom = ENV["ALGOLIA_CUSTOM_ENABLED"]
+    if (algoliaSearch || algoliaCustom)
+        Globals.putsColText(Globals::PURPLE,"Algolia is enabled - updating just-the-docs.js ...")
+        source = File.join(site.dest, 'assets/js/just-the-docs.js')
+        jtdJS = File.read(source)
+        jtdJS_upd = jtdJS.sub('initSearch();', '/*initSearch();*/')
+        FileUtilities.overwrite_file(source, jtdJS_upd)
+        Globals.moveUpOneLine
+        Globals.clearLine
+        Globals.putsColText(Globals::PURPLE,"Algolia is enabled - updating just-the-docs.js ... done")
+
+    end
+end
+
