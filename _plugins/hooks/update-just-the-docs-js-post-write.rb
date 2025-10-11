e9a9a96b4e5c91a5bb33ac76a186813e292cf670
Florin <florin_hoinarescu@yahoo.com>
2025-10-11 23:59:29 +0300

    update

--- _plugins/hooks/update-just-the-docs-js-post-write.rb
+++ _plugins/hooks/update-just-the-docs-js-post-write.rb
@@ -10,7 +10,13 @@ Jekyll::Hooks.register :site, :post_write do |site|
         Globals.putsColText(Globals::PURPLE,"Algolia is enabled - updating just-the-docs.js ...")
         source = File.join(site.dest, 'assets/js/just-the-docs.js')
         jtdJS = File.read(source)
+        
         jtdJS_upd = jtdJS.sub('initSearch();', '/*initSearch();*/')
+        jtdJS = jtdJS_upd
+        jtdJS_upd = jtdJS.sub('update();', '/*update();*/')
+        jtdJS = jtdJS_upd
+        jtdJS_upd = jtdJS.sub('setTimeout(update, 0);', '/*setTimeout(update, 0);*/')
+        
         FileUtilities.overwrite_file(source, jtdJS_upd)
         Globals.moveUpOneLine
         Globals.clearLine
